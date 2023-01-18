3d79df39429f3efbbb26f609007aff06ab8ff4c8
Ajebli_Ahmed <78688533+Ahmed-ajb@users.noreply.github.com>
2023-01-18 10:34:18 +0100

    update

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,25 +1,22 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "https://raw.githubusercontent.com/Ahmed-ajb/DW/main/3-%20ETL%20-%20Alimentation/dw%20talend%20screenshots/26%20job%20order%20fact.png?token=GHSAT0AAAAAAB5QK4ZP7VP72CKMD6DRNY2YY6HCUAQ",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/DW/main/3-%20ETL%20-%20Alimentation/dw%20talend%20screenshots/26%20job%20order%20fact.png?token=GHSAT0AAAAAAB5QK4ZPKFJBVFP6ZYUCMYSKY6HY2KQ",
     title: "ETL - talend open studio",
     description: " I use ETL process to move data from different sources to a central repository for further analysis.",
     tech_stack: "JAVA, talend open studio",
+    github_url: "https://github.com/Ahmed-ajb/DW",
+    demo_url: "https://github.com/Ahmed-ajb?tab=repositories",
+
   },
-  {
-    img: "https://user-images.githubusercontent.com/81421925/212548688-93895b90-62a6-41d4-b677-6fc75b918100.png",
-    title: "POS -SYMA with Scala",
-    description:
-      "A robust Point of Sale system that allows users to efficiently manage sales data and analyze it in an interactive dashboard using JavaFX, Scala and MySQL..",
-    tech_stack: "JAVAFX - SCALA ",
-    github_url: "https://github.com/Ahmed-ajb/scala_project.git",
-    },
   {
     img: "https://raw.githubusercontent.com/Ahmed-ajb/Towatch_project/main/images%20pour%20rapport/acceuil%20inde.png",
     title: "ToWatch",
     description: "Towatch est une application web de recommandation de films " ,
     tech_stack: "php, Html ,css , js , kmeans , R ",
     github_url: "https://github.com/Ahmed-ajb/Towatch_project.git",
+    demo_url: "https://github.com/Ahmed-ajb?tab=repositories",
+
   },
 ];
 
@@ -27,35 +24,31 @@ export const ProjectList = [
 
 export const stackList = [
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
-    name: "HTML",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/TalendLogoCoral.png?token=GHSAT0AAAAAAB5QK4ZP3GIH7MBRU62BSS6OY6HYSFQ",
+    name: "Talend studio",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
-    name: "CSS",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/Python-logo-notext.svg.png?token=GHSAT0AAAAAAB5QK4ZOBID5JEPXL6FGGPLKY6HYS4Q",
+    name: "Python",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
-    name: "JavaScript",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/R_logo.svg.png?token=GHSAT0AAAAAAB5QK4ZO7NDR5WJE36WSGVCIY6HYVFA",
+    name: "R",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
-    name: "ReactJS",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/1200px-Power_bi_logo_black.svg.png?token=GHSAT0AAAAAAB5QK4ZOS4ZYKHJBOGBX622OY6HYUSA",
+    name: "PowerBI",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
-    name: "Bootstrap",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/1200px-Java_Logo.svg.png?token=GHSAT0AAAAAAB5QK4ZO2WPNM6HLMWFVGWEIY6HYVRA",
+    name: "Java",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
-    name: "Scss",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/Hey_Machine_Learning_Logo.png?token=GHSAT0AAAAAAB5QK4ZOVC24QPWLJ45ZVZUSY6HYV6Q",
+    name: "M L",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
-    name: "Git",
-  },
-  {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
-    name: "Responsive",
+    img: "https://raw.githubusercontent.com/Ahmed-ajb/portfolio/main/src/images/Visualization512.png?token=GHSAT0AAAAAAB5QK4ZOWCH6GLDZR5PTAHA4Y6HYX6A",
+    name: "visualisation",
   },
 ];
