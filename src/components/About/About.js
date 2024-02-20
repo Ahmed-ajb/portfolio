ab3255371782cc00c851b1278d524609abde0989
Ajebli_Ahmed <78688533+Ahmed-ajb@users.noreply.github.com>
2024-02-20 19:39:08 +0000

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,7 +20,7 @@ function About() {
           />
           <div className="AboutBio">
 
-I am Ahmed Ajebli, a dedicated and ambitious student of Systems and Big Data with hands-on experience in ETL, Java, and IBM SPSS. I am currently pursuing my professional degree in Systems and Big Data at the Ecole Supérieure de Technologie de Salé. My passion is using my technical skills to solve real-world problems and I am always eager to learn new technologies. I have a proven track record of success, having completed internships with La Province and Allianz. In my free time, I enjoy staying up to date with the latest developments in the tech industry and I am a keen networker.
+I am Ahmed Ajebli, a dedicated and ambitious student of Systems and Big Data with hands-on experience in ETL, Java, and IBM SPSS. I am currently pursuing my master degree in IA and Data analysis at the FS MEKNES . My passion is using my technical skills to solve real-world problems and I am always eager to learn new technologies. I have a proven track record of success, having completed internships with La Province and Allianz. In my free time, I enjoy staying up to date with the latest developments in the tech industry and I am a keen networker.
           </div>
           <div className="AboutBio tagline2">
             I have become confident using the following technologies.
